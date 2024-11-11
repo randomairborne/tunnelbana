e8dbcb4b760640d8e1c4d49f69af8b9d0cfeccb1
valkyrie_pilot <valk@randomairborne.dev>
2024-11-11 16:17:18 -0700

    Document hidepaths

--- crates/tunnelbana-hidepaths/src/lib.rs
+++ crates/tunnelbana-hidepaths/src/lib.rs
@@ -1,4 +1,26 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+//! # tunnelbana-hidepaths
+//! Hide specific paths in tower services by sending them to a 404 service.
+//!
+//! Part of the [tunnelbana](https://github.com/randomairborne/tunnelbana) project.
+//!
+//! # Example
+//! ```rust
+//! use tower_http::services::ServeDir;
+//! use tower::{ServiceBuilder, ServiceExt};
+//! use http::Response;
+//! use tunnelbana_hidepaths::HidePathsLayer;
+//!
+//! let hidepaths_middleware = HidePathsLayer::builder()
+//!     .hide("/_redirects")
+//!     .hide_all(["/.htaccess", "/.well-known/{*hide}"])
+//!     .build()
+//!     .expect("Failed to build path hide router");
+//! let serve_dir = ServeDir::new("/var/www/html").append_index_html_on_directories(true);
+//! let service = ServiceBuilder::new()
+//!    .layer(hidepaths_middleware)
+//!    .service(serve_dir);
+//! ```
 use std::{
     convert::Infallible,
     future::Future,
@@ -14,31 +36,39 @@ pub use matchit::InsertError;
 use tower::{Layer, Service};
 
 #[derive(Clone)]
-pub struct HidePathLayerBuilder<N = DefaultNotFoundService> {
+/// Build a [`matchit::Router`] of paths which should be routed to
+/// the not found service.
+///
+/// The not found service defaults to [`DefaultNotFoundService`],
+/// however it is very barebones, so it is reccomended to supply your own with [`Self::with_not_found_service`].
+pub struct HidePathsLayerBuilder<N = DefaultNotFoundService> {
     hidden: matchit::Router<()>,
-    fallback_svc: N,
+    notfound: N,
     errors: Vec<(String, InsertError)>,
 }
 
-impl<N> HidePathLayerBuilder<N> {
+impl<N> HidePathsLayerBuilder<N> {
     #[must_use]
-    pub fn new() -> HidePathLayerBuilder<DefaultNotFoundService> {
-        HidePathLayerBuilder {
+    /// Create a new builder with the [`DefaultNotFoundService`].
+    pub fn new() -> HidePathsLayerBuilder<DefaultNotFoundService> {
+        HidePathsLayerBuilder {
             hidden: matchit::Router::new(),
-            fallback_svc: DefaultNotFoundService,
+            notfound: DefaultNotFoundService,
             errors: Vec::new(),
         }
     }
 
-    pub fn with_fallback<T>(self, fallback_svc: T) -> HidePathLayerBuilder<T> {
-        HidePathLayerBuilder {
-            fallback_svc,
+    /// Use a different service for 404'd files than the [`DefaultNotFoundService`].
+    pub fn with_not_found_service<T>(self, notfound: T) -> HidePathsLayerBuilder<T> {
+        HidePathsLayerBuilder {
+            notfound,
             hidden: self.hidden,
             errors: self.errors,
         }
     }
 
     #[must_use]
+    /// All [`matchit`] routes passed to this method will be routed to the not found service.
     pub fn hide(mut self, route: impl Into<String>) -> Self {
         let route = route.into();
         if let Err(err) = self.hidden.insert(&route, ()) {
@@ -48,6 +78,7 @@ impl<N> HidePathLayerBuilder<N> {
     }
 
     #[must_use]
+    /// Convenience method for calling [`Self::hide`] in a loop.
     pub fn hide_all<IS: Into<String>>(mut self, routes: impl IntoIterator<Item = IS>) -> Self {
         for route in routes {
             self = self.hide(route);
@@ -55,39 +86,42 @@ impl<N> HidePathLayerBuilder<N> {
         self
     }
 
+    /// Get a list of errors which have occured inside the builder.
     pub fn errors(&self) -> &[(String, InsertError)] {
         self.errors.as_slice()
     }
 
-    /// Build this [`HidePathLayer`].
+    /// Build this [`HidePathsLayer`].
     /// # Errors
     /// This function errors if matchit has had any errors while inserting-
     /// you get the path that was inserted, and the error.
-    pub fn build(self) -> Result<HidePathLayer<N>, Vec<(String, InsertError)>> {
+    pub fn build(self) -> Result<HidePathsLayer<N>, Vec<(String, InsertError)>> {
         if !self.errors.is_empty() {
             return Err(self.errors);
         }
-        Ok(HidePathLayer {
+        Ok(HidePathsLayer {
             hidden: Arc::new(self.hidden),
-            fallback_svc: self.fallback_svc,
+            notfound: self.notfound,
         })
     }
 }
 
 #[derive(Clone)]
-pub struct HidePathLayer<N = DefaultNotFoundService> {
+/// A [`tower::Layer`] for use with a [`tower::ServiceBuilder`] to reply with a fallback
+/// service to any routes found internally.
+pub struct HidePathsLayer<N = DefaultNotFoundService> {
     hidden: Arc<matchit::Router<()>>,
-    fallback_svc: N,
+    notfound: N,
 }
 
-impl HidePathLayer<DefaultNotFoundService> {
+impl HidePathsLayer<DefaultNotFoundService> {
     #[must_use]
-    pub fn builder() -> HidePathLayerBuilder<DefaultNotFoundService> {
-        HidePathLayerBuilder::<DefaultNotFoundService>::new()
+    pub fn builder() -> HidePathsLayerBuilder<DefaultNotFoundService> {
+        HidePathsLayerBuilder::<DefaultNotFoundService>::new()
     }
 }
 
-impl<S, N> Layer<S> for HidePathLayer<N>
+impl<S, N> Layer<S> for HidePathsLayer<N>
 where
     N: Clone,
 {
@@ -96,13 +130,15 @@ where
     fn layer(&self, inner: S) -> HidePath<S, N> {
         HidePath {
             hidden: self.hidden.clone(),
-            notfound: self.fallback_svc.clone(),
+            notfound: self.notfound.clone(),
             inner,
         }
     }
 }
 
 #[derive(Clone)]
+/// A wrapper service which forwards to one of two inner services based on if the requested
+/// path is contained within its internal router.
 pub struct HidePath<S, N> {
     hidden: Arc<matchit::Router<()>>,
     notfound: N,
@@ -110,6 +146,8 @@ pub struct HidePath<S, N> {
 }
 
 #[pin_project::pin_project(project = PinResponseSource)]
+/// Future which always delegates the whole response to either the default service, or
+/// a not-found fallback, and returns the service response unmodified.
 pub enum ResponseFuture<S, N> {
     Child(#[pin] S),
     NotFound(#[pin] N),
@@ -174,8 +212,10 @@ where
 }
 
 #[derive(Clone, Copy, Debug, Default)]
+/// Unconfigurable service which returns HTTP 404s with no body.
 pub struct DefaultNotFoundService;
 
+/// Future type which returns an empty HTTP 404.
 pub struct DefaultNotFoundFuture;
 
 impl<T> Service<T> for DefaultNotFoundService {
