6716037fe8345844103bb2ee84692033561e8843
valkyrie_pilot <valk@randomairborne.dev>
2025-06-21 15:49:10 -0600

    remove spurious copy

--- ci/debpkg.sh
+++ ci/debpkg.sh
@@ -12,4 +12,3 @@ mkdir $WORKDIR/DEBIAN
 ./ci/gencontrol.sh $VERSION $ARCHNAME > $WORKDIR/DEBIAN/control
 mkdir -p $WORKDIR/usr/bin $WORKDIR/usr/share/tunnelbana
 cp target/release/tunnelbana $WORKDIR/usr/bin
-cp -r assets $WORKDIR/usr/share/tunnelbana
